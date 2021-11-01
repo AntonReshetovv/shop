9e40241a38cb71f5b76115da4179e204ade97526
a.reshetov <a.reshetov@12storeez.com>
2021-11-01 19:40:33 +0700

    Изменил роутинг, теперь мои компоненты которые отрисовываются в authLayout имеют статус children

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from "./auth/login/login.component";
 import {RegistrationComponent} from "./auth/registration/registration.component";
 import {CatalogComponent} from "./catalog/catalog.component";
+import {AuthLayoutComponent} from "./auth/shared/layout/auth-layout/auth-layout.component";
 
 const routes: Routes = [
-  {path: '', redirectTo: '/api/login', pathMatch: 'full'},
-  {path: 'api/login', component: LoginComponent},
-  {path: 'api/registration', component: RegistrationComponent},
+  {
+    path: '', component: AuthLayoutComponent, children: [
+      {path: '', redirectTo: '/api/login', pathMatch: 'full'},
+      {path: 'api/login', component: LoginComponent},
+      {path: 'api/registration', component: RegistrationComponent},
+    ]
+  },
   {path: 'api/catalog', component: CatalogComponent}
 ];
 
@@ -15,4 +20,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
