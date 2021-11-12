0a3254fb7c8d95d717d0ee617d520fe05f0c41bc
a.reshetov <a.reshetov@12storeez.com>
2021-11-12 16:55:11 +0700

    Форматирование catalog component и product-form

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,19 +1,17 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {MatTableDataSource} from "@angular/material/table";
-import {MatSort} from "@angular/material/sort";
-import {MatPaginator} from "@angular/material/paginator";
-import {map} from "rxjs/operators";
-import {ProductItem} from "../shared/interfaces/product-item";
-import {HttpService} from "../shared/services/http.service";
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+import { map } from 'rxjs/operators';
+import { ProductItem } from '../shared/interfaces/product-item';
+import { HttpService } from '../shared/services/http.service';
 
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
-  styleUrls: ['./catalog.component.scss']
+  styleUrls: ['./catalog.component.scss'],
 })
-
 export class CatalogComponent implements OnInit {
-
   products: ProductItem[] = [];
 
   displayedColumns: string[] = ['title', 'price', 'action'];
@@ -22,24 +20,30 @@ export class CatalogComponent implements OnInit {
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  constructor(private httpService: HttpService) {
-  }
+  constructor(private httpService: HttpService) {}
 
   ngOnInit(): void {
-    this.getAll()
+    this.getAll();
   }
 
-  getAll(){
-    this.httpService.getAllProduct().snapshotChanges().pipe(
-      map((changes) => (
-        changes.map((data) => ({key: data.payload.key, ...data.payload.val()}))
-      ))
-    ).subscribe((data) => {
-      this.products = data;
-      this.dataSource = new MatTableDataSource(this.products)
-      this.dataSource.sort = this.sort;
-      this.dataSource.paginator = this.paginator;
-    })
+  getAll() {
+    this.httpService
+      .getAllProduct()
+      .snapshotChanges()
+      .pipe(
+        map((changes) =>
+          changes.map((data) => ({
+            key: data.payload.key,
+            ...data.payload.val(),
+          }))
+        )
+      )
+      .subscribe((data) => {
+        this.products = data;
+        this.dataSource = new MatTableDataSource(this.products);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      });
   }
 
   applyFilter(event: Event) {
@@ -47,9 +51,10 @@ export class CatalogComponent implements OnInit {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
-  removeProduct(product: ProductItem){
-    this.httpService.deleteProduct(product)
+  removeProduct(product: ProductItem) {
+    this.httpService
+      .deleteProduct(product)
       .then(() => console.log('Продукт удален'))
-      .catch(err => console.log('Возникла ошибка', err))
+      .catch((err) => console.log('Возникла ошибка', err));
   }
 }
